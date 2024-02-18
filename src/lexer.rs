3d9de63d01429fa54bb2981335f976dfc5874f0d
season1618 <seasonminority314@gmail.com>
2024-02-18 17:28:08 +0900

    空白を飛ばす

--- src/lexer.rs
+++ src/lexer.rs
@@ -17,6 +17,31 @@ impl<'a> Lexer<'a> {
     }
 
     fn tokenize(&mut self) -> Vec<Token<'a>> {
-        vec![StrLit(self.chs)]
+        let mut tokens = Vec::new();
+        while let Some(c) = self.peek_char() {
+            if c.is_whitespace() {
+                self.next_char();
+                continue;
+            }
+            tokens.push(StrLit(self.chs));
+            break;
+        }
+        tokens
+    }
+
+    fn peek_char(&self) -> Option<char> {
+        if let Some(c) = self.chs.chars().nth(0) {
+            return Some(c);
+        }
+        None
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        if let Some(c) = self.chs.chars().nth(0) {
+            let i = if let Some((i, _)) = self.chs.char_indices().nth(1) { i } else { self.chs.len() };
+            self.chs = &self.chs[i..];
+            return Some(c);
+        }
+        None
     }
 }
