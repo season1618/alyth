39e129edf5c8217d72a683d88aea1d1315d995ed
season1618 <seasonminority314@gmail.com>
2024-03-01 20:20:59 +0900

    ファイルに出力

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,19 +1,36 @@
+use std::fs::File;
+use std::io::{self, Write};
+
 use crate::data::*;
 
 use Expr::*;
 
-pub fn gen_program(expr: Expr) {
-    println!(".intel_syntax noprefix");
-    println!(".global main");
-    println!("main:");
-    gen_expr(expr);
-    println!("    pop rax");
-    println!("    ret");
+pub fn gen_program<'a>(node: Expr, dest: &'a mut File) {
+    let mut codegen = CodeGen::new(dest);
+    codegen.gen_program(node);
 }
 
-fn gen_expr(expr: Expr) {
-    match expr {
-        Num(val) => { println!("    push {val}"); },
-        _ => {},
+struct CodeGen<'a> {
+    dest: &'a mut File,
+}
+
+impl<'a> CodeGen<'a> {
+    fn new(dest: &'a mut File) -> Self {
+        Self { dest }
+    }
+
+    fn gen_program(&'a mut self, expr: Expr) -> Result<(), io::Error> {
+        writeln!(self.dest, ".intel_syntax noprefix")?;
+        writeln!(self.dest, ".global main")?;
+        writeln!(self.dest, "main:")?;
+        self.gen_expr(expr)?;
+        writeln!(self.dest, "    pop rax")?;
+        writeln!(self.dest, "    ret")
+    }
+    
+    fn gen_expr(&mut self, expr: Expr) -> Result<(), io::Error> {
+        match expr {
+            Num(val) => writeln!(self.dest, "    push {val}"),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod parser;
 mod codegen;
 
 use std::env;
-use std::fs;
+use std::fs::{self, File};
 
 use crate::lexer::tokenize;
 use crate::parser::parse;
@@ -13,15 +13,14 @@ use crate::codegen::gen_program;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let src_path = &args[1];
+    let dest_path = &args[2];
 
-    let Ok(code) = fs::read_to_string(src_path) else {
-        println!("could not open the source file.");
-        return;
-    };
+    let code = fs::read_to_string(src_path).expect("failed to open the source file");
+    let mut dest = File::create(dest_path).expect("failed to create the destination file");
 
     let tokens = tokenize(&code);
     eprintln!("{:?}", tokens);
     let nodes = parse(tokens);
     eprintln!("{:?}", nodes);
-    gen_program(nodes);
+    gen_program(nodes, &mut dest);
 }
